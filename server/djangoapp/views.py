3eef1333b37af3bd8b4452a2aaa4f318c883727a
Xiaozhen2022 <xiaozhen@gmail.com>
2024-07-21 17:06:56 -0400

    changes made from theia lab environment

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -83,6 +83,7 @@ def registration(request):
 # a list of dealerships
 # def get_dealerships(request):
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+@csrf_exempt
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
@@ -93,6 +94,7 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
+@csrf_exempt
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
@@ -117,6 +119,8 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
+
+@csrf_exempt
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,13 +30,13 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = [
-    "https://xiaozhen-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    "https://xiaozhen-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
     ".localhost", 
     '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost',
     'http://127.0.0.1',
-    'https://xiaozhen-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://xiaozhen-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_COOKIE_SECURE = True
